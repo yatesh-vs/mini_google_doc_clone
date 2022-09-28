fccd8bb1ca8b6d0dc25f3ea9612e1ee530817f7c
yatesh-vs <100896027+yatesh-vs@users.noreply.github.com>
2022-09-28 13:53:14 +0530

    Add files via upload

---
+++ precache-manifest.c50c72db95b9ff35a65a11274ed7b536.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251b5fc2bc17e1ab35eb364ff80f9c09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d634ccd64bda80844b5",
+    "url": "/static/js/2.6e2aeedb.chunk.js"
+  },
+  {
+    "revision": "a9a5798a3ffc6085348a",
+    "url": "/static/js/main.4d44b188.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
