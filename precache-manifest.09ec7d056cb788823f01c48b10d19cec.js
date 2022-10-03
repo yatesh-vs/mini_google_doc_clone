05542a621d3482c699db076ed1630dcea62a2ba4
yatesh-vs <100896027+yatesh-vs@users.noreply.github.com>
2022-10-03 18:58:45 +0530

    Add files via upload

---
+++ precache-manifest.09ec7d056cb788823f01c48b10d19cec.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f6cb94893b113f7c97d2237cde33639",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3de8046e6947656528a4",
+    "url": "/static/js/2.eafe52c2.chunk.js"
+  },
+  {
+    "revision": "5a4f8bbef38d21224115",
+    "url": "/static/js/main.9b61b1a7.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6c86212b8dfe940d1d9da414775f6161.js"
+  "/precache-manifest.09ec7d056cb788823f01c48b10d19cec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
